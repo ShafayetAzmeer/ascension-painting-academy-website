7a8bd1ce0ae4b609d35ee7426c6187c895c209d7
ShafayetAzmeer <shafayetazmeer@gmail.com>
2021-10-05 23:50:25 +0600

    assignment-9-completed-updated

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>404</h1>
+            <h1 className="m-5 p5 fs-1 fw-bold">404</h1>
+            <p className="m-5 p-5 fs-1 fw-bold">Page Not Found</p>
         </div>
     );
 };
